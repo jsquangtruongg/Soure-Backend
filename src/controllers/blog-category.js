3b2fa59bdae9ffb8fedfaf6fbdea6803aaab9a9e
jstruongg <nguyenqtthangbinh@gmail.com>
2024-10-03 11:17:09 +0700

    feature-fillter/bogCategory

--- src/controllers/blog-category.js
+++ src/controllers/blog-category.js
@@ -3,7 +3,15 @@ import { InternalServerError, badRequest } from "../middlewares/handle_error";
 import * as services from "../services";
 export const getAllBlogCategory = async (req, res) => {
   try {
-    const response = await services.getAllBlogCategory();
+    const { title, describe, lastName, ...body } = req.query;
+
+    const response = await services.getAllBlogCategory(
+      title,
+      describe,
+      lastName, 
+      body
+    );
+
     if (response.err === 1) return badRequest("ERROR", res);
     return res.status(200).json(response);
   } catch (error) {
