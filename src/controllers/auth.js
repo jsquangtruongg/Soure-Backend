88d35506417cd6c0a59a75e95c86654e43afb6ce
BaViet12 <hpa220377@gmail.com>
2024-12-28 22:09:52 +0700

    BE DANG NHAP up

---
+++ src/controllers/auth.js
@@ -0,0 +1,20 @@
+import * as services from "../services";
+import { InternalServerError, badRequest } from "../middlewares/handle_error";
+import { email, password, firstName, lastName } from "../helpers/joi_schema";
+import Joi from "joi";
+
+
+export const login = async (req, res) => {
+  try {
+    const { error } = Joi.object({
+      email,
+      password,
+    }).validate(req.body);
+    if (error) return badRequest(error.details[0]?.message, res);
+    const response = await services.login(req.body);
+    if (response.err === 1) return badRequest(response.mes, res);
+    return res.status(200).json(response);
+  } catch (error) {
+    return InternalServerError(res);
+  }
+};
